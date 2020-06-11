d61ae26f967b3ec70822c4d293a4e77e590668a3
Chuckz Okoye <tricelex@gmail.com>
2020-06-11 11:34:27 +0100

    pages styled using bootstrap 4

--- generator/views.py
+++ generator/views.py
@@ -1,3 +1,28 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+import random
+
 
 # Create your views here.
+def home(request):
+    return render(request, 'generator/home.html', {'password': 'sdhkhdsdvi334oasd'})
+
+
+def password(request):
+    the_password = ''
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+    length = int(request.GET.get('length', 12))
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&*()'))
+    if request.GET.get('numbers'):
+        characters.extend(list('0123456789'))
+    for x in range(length):
+        the_password += random.choice(characters)
+    return render(request, 'generator/password.html', {'password': the_password})
+
+
+def about(request):
+    return render(request, 'generator/about.html', {'title': 'About Us'})
--- password_generator/urls.py
+++ password_generator/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from generator import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('mopol/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('password/', views.password, name='password'),
+    path('about/', views.about, name='about'),
 ]
